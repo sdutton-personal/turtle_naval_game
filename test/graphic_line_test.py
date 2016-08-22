4deb02b9ab6ed7965b4a143fd744af2c99befefe
Shawn Dutton <shawndutton1@gmail.com>
2016-08-22 05:19:40 -0700

    updated graphic_line_test adding new cases

--- test/graphic_line_test.py
+++ test/graphic_line_test.py
@@ -84,8 +84,7 @@ class TestGraphicLines(TurtleNavalTestBase):
         end_pt = (-12, -38)
 
         # points on the line
-        # note this first one will actually be to the right of the line very slightly because of rounding
-        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=start_pt, exp_y=start_pt[1], exp_side=2)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=start_pt, exp_y=start_pt[1], exp_side=1)
         self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=end_pt, exp_y=end_pt[1], exp_side=1)
         self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(-6, -20), exp_y=-20, exp_side=1)
 
@@ -111,6 +110,40 @@ class TestGraphicLines(TurtleNavalTestBase):
         self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(-10, -32.5), exp_y=-32, exp_side=2)
         self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(-7, -31), exp_y=-23, exp_side=2)
         self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(-5, -18), exp_y=-17, exp_side=2)
+
+    def test_point_location_and_expected_side_function_in_q4(self):
+        # test line creation and side function in q4, (x positive and y negative)
+        start_pt = (5, -75)
+        end_pt = (10, -25)
+
+        # points on the line
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=start_pt, exp_y=start_pt[1], exp_side=1)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=end_pt, exp_y=end_pt[1], exp_side=1)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(6, -65), exp_y=-65, exp_side=1)
+
+        # points that do not intersect our line on the x axis at all ( y point is less than or greater than both the
+        # start and the end point y values.
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(8, -24), exp_y=-45, exp_side=0)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(start_pt[0], -24.999), exp_y=start_pt[1], exp_side=0)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(end_pt[0], -75.0001), exp_y=end_pt[1], exp_side=0)
+
+        # points where the x axis would intersect our line, but it is less than or greater than
+        # both x points on the line
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(4.999, -30), exp_y=-75.01, exp_side=1)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(4, start_pt[1]), exp_y=-85, exp_side=1)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(11, end_pt[1]), exp_y=-15, exp_side=2)
+
+        # points where the x axis would intersect, and the x is in between our two points.
+        # points on the left side
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(6, -64), exp_y=-65, exp_side=1)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(7.465, -40), exp_y=-50.35, exp_side=1)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(9, -28), exp_y=-35, exp_side=1)
+        # points on the right side
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(8, -50), exp_y=-45, exp_side=2)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(9, -36.254), exp_y=-35, exp_side=2)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(7, -56), exp_y=-55, exp_side=2)
+        self.run_line_tst(start_pt=start_pt, end_pt=end_pt, test_pt=(6, -70), exp_y=-65, exp_side=2)
+
 
 
 if __name__ == '__main__':
