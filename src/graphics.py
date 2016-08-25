c542f33dd5b0e72044c6df3308d65e8d82a735ef
Shawn Dutton <shawndutton1@gmail.com>
2016-08-25 05:52:10 -0700

    updated graphicShape class adding method to check for shape being out of bounds

--- src/graphics.py
+++ src/graphics.py
@@ -317,6 +317,9 @@ class GraphicShape(object):
         self._scale = scale
         self._scale_width = self._overall_width + (self._overall_width * self._scale / 100)
         self._scale_length = self._overall_length + (self._overall_length * self._scale / 100)
+        self.shape_out_of_bounds = False
+        self.x_boundary = None
+        self.y_boundary = None
 
         self.load_graphic_lines()
         self.load_points_to_draw()
@@ -354,6 +357,24 @@ class GraphicShape(object):
         else:
             return False
 
+    def is_shape_out_of_bounds(self, x_max, y_max):
+        x_min = x_max * -1
+        y_min = y_max * -1
+        result_lst = [0, 0]
+        for line in self.graphic_lines_lst:
+            if line.start_point_cur_loc[0] > x_max or line.end_point_cur_loc[0] > x_max:
+                result_lst[0] = 1
+            if line.end_point_cur_loc[0] < x_min or line.end_point_cur_loc[0] < x_min:
+                result_lst[0] = -1
+            if line.start_point_cur_loc[1] > y_max or line.end_point_cur_loc[1] > y_max:
+                result_lst[1] = 1
+            if line.start_point_cur_loc[1] < y_min or line.end_point_cur_loc[1] < y_min:
+                result_lst[1] = -1
+        if result_lst[0] or result_lst[1]:
+            return result_lst
+        else:
+            return False
+
 
 class BowShape(GraphicShape):
 
@@ -376,10 +397,10 @@ class HullShape(GraphicShape):
     line_color = 'black'
 
     def load_graphic_lines(self):
-        bow_starboard_point = (self._scale_length / self._shape_to_overall_scale, -self._scale_width / 2)
-        stern_st_starboard_point = (-self._scale_length / self._shape_to_overall_scale, -self._scale_width / 2)
-        stern_port_point = (-self._scale_length / self._shape_to_overall_scale, self._scale_width / 2)
-        bow_port_point = (self._scale_length / self._shape_to_overall_scale, self._scale_width / 2)
+        bow_starboard_point = GraphicPoint(self._scale_length / self._shape_to_overall_scale, -self._scale_width / 2)
+        stern_st_starboard_point = GraphicPoint(-self._scale_length / self._shape_to_overall_scale, -self._scale_width / 2)
+        stern_port_point = GraphicPoint(-self._scale_length / self._shape_to_overall_scale, self._scale_width / 2)
+        bow_port_point = GraphicPoint(self._scale_length / self._shape_to_overall_scale, self._scale_width / 2)
 
         self.graphic_lines_lst.append(GraphicLine(bow_starboard_point, stern_st_starboard_point))
         self.graphic_lines_lst.append(GraphicLine(stern_st_starboard_point, stern_port_point))
@@ -393,10 +414,10 @@ class SternShape(GraphicShape):
     line_color = 'black'
 
     def load_graphic_lines(self):
-        stern_hull_port_point = (-self._scale_length / self._shape_to_overall_scale, self._scale_width / 2)
-        stern_end_port_point = (-self._scale_length / 2.5, self._scale_width / 3)
-        stern_end_starboard_point = (-self._scale_length / 2.5, -self._scale_width / 3)
-        stern_hull_starboard_point = (-self._scale_length / self._shape_to_overall_scale, -self._scale_width / 2)
+        stern_hull_port_point = GraphicPoint(-self._scale_length / self._shape_to_overall_scale, self._scale_width / 2)
+        stern_end_port_point = GraphicPoint(-self._scale_length / 2.5, self._scale_width / 3)
+        stern_end_starboard_point = GraphicPoint(-self._scale_length / 2.5, -self._scale_width / 3)
+        stern_hull_starboard_point = GraphicPoint(-self._scale_length / self._shape_to_overall_scale, -self._scale_width / 2)
 
         self.graphic_lines_lst.append(GraphicLine(stern_hull_port_point, stern_end_port_point))
         self.graphic_lines_lst.append(GraphicLine(stern_end_port_point, stern_end_starboard_point))
